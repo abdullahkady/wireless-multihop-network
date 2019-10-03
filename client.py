9cf5f2345d2bc45c8e4aee697ee66937df7f077c
Abdullah Kady <abdullahkady@gmail.com>
2019-10-03 17:30:01 +0200

    Add forwarding

--- client.py
+++ client.py
@@ -1,40 +1,66 @@
 from bluetooth_connector import Bluetoothctl, BluetoothctlError
 import bluetooth
 import inquirer
+import json
+
+def send_to_display(target_display_name, data):
+    nearby_devices = bluetooth.discover_devices(lookup_names=True)
+    target_mac = None
+    for mac, display in nearby_devices:
+        if display == answers["device"]:
+            target_mac = mac
+            break
+    if target_mac is None:
+        raise Exception('NOT FOUND')
+    send_to(target_mac)
+
+def send_to(target_mac, data):
+    print('Connecting to {}'.format(target_mac))
+    connector = Bluetoothctl()
+    connector.connect(target_mac)
+    port = 1
+
+    socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    socket.connect((target_mac, port))
+    print('Successfully connected to {}'.format(target_mac))
+
+    socket.send(json.dumps(data))
+    print('Sent data successfully')
+    socket.close()
+
 
 def choose_user_to_connect():
+    # Returns the MAC
     nearby_devices = bluetooth.discover_devices(lookup_names=True)
     questions = [
         inquirer.List('device',
-            message="Choose device to connect?",
+            message="Choose immediate device to connect",
             choices=[x for _, x in nearby_devices],
         ),
     ]
-    answers = inquirer.prompt(questions)
+    answers = inquirer.prompt(questions)['device']
     for mac, display in nearby_devices:
         if display == answers["device"]:
             return mac
 
-target_mac = choose_user_to_connect()
 
-print('Connecting ...')
-connector = Bluetoothctl()
-connector.connect(target_mac)
+def initiate_client():
+    all_devices = ['mustafagoudah-Lenovo-Z51-70', 'eark']
+    questions = [
+        inquirer.List('device',
+            message="Choose target device to send to",
+            choices=all_devices,
+        ),
+    ]
+    actual_target = inquirer.prompt(questions)['device']
+    print('Discovering in range devices ...')
+    immediate_target = choose_user_to_connect()
+    payload = {
+        'sender': 'SENDER',
+        'receiver': actual_target,
+        'message': raw_input('Enter message to send')
+    }
+    send_to(immediate_target, data)
 
-port = 1
-
-socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-socket.connect((target_mac, port))
-
-while True:
-    user_input = raw_input('You: ')
-
-    if user_input == 'q':
-        socket.close()
-        print('bye')
-        break
-
-
-    socket.send(user_input)
-    data = socket.recv(1024)
-    print('Server: {}'.format(data))
+if __name__ == '__main__':
+    initiate_client()
--- server.py
+++ server.py
@@ -1,17 +1,38 @@
 import bluetooth
+import json
 
-server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+from client import send_to_display
 
-port = 1
-server_sock.bind(("",port))
-server_sock.listen(1)
-print('Server started')
-client_sock, address = server_sock.accept()
-print('Accepted connection from {}'.format(address))
-while True:
-    data = client_sock.recv(1024)
-    print('Got: {}'.format(data))
-    client_sock.send('ECHO: {}'.format(data))
+def start_server():
 
-client_sock.close()
-server_sock.close()
+    def forward_message(data):
+        receiver = data['receiver']
+        data['receiver'] = '*'
+        # Launch a client, connect to 'receiver', send data
+        send_to_display(receiver, data)
+
+    def handle_message(data):
+        receiver = data.get('receiver')
+        sender = data.get('sender')
+        message = data.get('message')
+        if receiver == '*':
+            print('{}: {}'.format(sender, message))
+        else:
+            forward_message(data)
+
+    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    port = 1
+    server_sock.bind(("", port))
+    server_sock.listen(1)
+    print('Server started')
+
+    while True:
+        client_sock, address = server_sock.accept()
+        print('Accepted connection from {}'.format(address))
+        data = json.loads(client_sock.recv(1024))
+        handle_message(data)
+
+    client_sock.close()
+    server_sock.close()
+
+start_server()
